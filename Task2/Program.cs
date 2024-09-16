3984c1dc241da011048599087ed22b2d8a6fd138
Efremova Aleksandra <doctor.akab@gmail.com>
2024-09-16 22:05:05 +0300

    PracticSeminar7 : Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу вычисления функции
+// Аккрмана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+
+void Main()
+{
+    int M = ReadInt("Введите первое положительное число: ");
+    int N = ReadInt("Введите второе положительное число: ");
+    int res = PrintAkkerman(M, N);
+    System.Console.WriteLine(res);
+}
+
+int PrintAkkerman(int M, int N)
+{
+    if(M == 0) return N + 1;
+    else
+        if(M > 0 && N == 0) return PrintAkkerman(M - 1, 1);
+    else
+        if(M > 0 && N > 0) return PrintAkkerman(M - 1, PrintAkkerman(M, N - 1));   
+    return 0;
+}
+
+// метод для ввода числа пользователем
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
