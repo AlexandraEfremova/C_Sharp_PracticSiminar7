6e992c99b4be3346a2ffa524ca0f6841a11cb1bf
Efremova Aleksandra <doctor.akab@gmail.com>
2024-09-16 22:22:56 +0300

    PracticSeminar7 : Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте произвольный массив. Выведите его элементы, начиная с конца
+// Использовать рекурсию, не использовать циклы.
+
+
+void Main()
+{
+    int[] array = {1, 3, 545, 28, 53} ;
+    ReverseArray(array, array.Length - 1);
+}
+
+void ReverseArray(int[] array, int i)
+{
+    if(i < 0) return;
+    System.Console.Write(array[i]);
+    if(i > 0)System.Console.Write(", ");
+    ReverseArray(array, i - 1);
+}
+
+Main();
